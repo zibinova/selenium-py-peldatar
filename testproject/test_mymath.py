c93d59e194ee786c666f6d499312b7f8535532c1
Novák Ibolya <64532169+zibinova@users.noreply.github.com>
2021-06-11 09:41:00 +0200

    Create test_mymath.py

---
+++ testproject/test_mymath.py
@@ -0,0 +1,17 @@
+import pytest
+
+from mymath import add_numbers
+
+def test_add_positive():
+    assert add_numbers(1, 2) == 3
+
+def test_add_zero():
+    assert add_numbers(1, 0) == 1
+
+def test_add_negative():
+    assert add_numbers(4, -100) == -96
+
+def test_add_string__expect_exception():
+    with pytest.raises(TypeError):
+        add_numbers(4, 'I DO NOT BELONG HERE')
+© 2021 GitHub, Inc.
