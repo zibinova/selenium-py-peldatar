c02877f7925a7a9a539b779eabbbc4b95a7199aa
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-06-17 13:06:32 +0200

    adding

--- python-homework/bartender.py
+++ python-homework/bartender.py
@@ -1,3 +1,31 @@
-x = input("Hány éves vagy?")
-y = input("Mit innál?")
+#ennel krealok egy meg jobbat, ez egy kicsit fapadosnak tűnik
+
+x = input("Hány éves?")
+y = input("Mit iszik?")
+
+if int(x) < 18:
+    if y == "sört":
+       print("Nem kap")
+    elif y == "kólát":
+        print("Parancsoljon a kólája!")
+    else:
+        print("Csak sör és kóla van!")
+if int(x) >= 60:
+    if y == "sört":
+        print("Parancsoljon a söre!")
+    elif y == "kólát":
+        print("A koffein megemelheti a vérnyomását!")
+    else:
+        print("Csak sör és kóla van!")
+elif 18 <= int(x) < 60:
+    if y == "sört":
+        print("Parancsoljon a söre!")
+
+    elif y == "kólát":
+        print("Parancsoljon a kólája!")
+    else:
+        print("Csak sör és kóla van!")
+
+
+
 
