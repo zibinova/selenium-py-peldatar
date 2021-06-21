a28249d968ca52e19e85e020637ae739b781b38c
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-06-21 15:33:01 +0200

    updated main.py and adding start.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-#driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+# driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://python.org")
 search_field = driver.find_element_by_name('q')
---
+++ selenium-homework/start.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("http://semjenhaza.hu/")
+
+
+
+
+
