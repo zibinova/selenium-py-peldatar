c59c97f1b2a8b07e83c0463ba10e0db109a10495
Novák Ibolya <64532169+zibinova@users.noreply.github.com>
2021-06-21 11:54:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-#options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 #driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://python.org")
