8980592bece6a9c29e64870f8849fdf0cf68d502
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-06-23 18:06:09 +0200

    updating webdriver_test.py

--- testproject/webdriver_test.py
+++ testproject/webdriver_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 #driver = webdriver.Firefox()
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
+
+
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
 elem.clear()
@@ -11,3 +14,7 @@ elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
+
+
+
+
