98049f055e5e8fd067474c6ad7930e67ae95fb59
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-06-24 15:55:54 +0200

    updating seleniumex.py

--- selenium-homework/seleniumex.py
+++ selenium-homework/seleniumex.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
 
 driver.get("https://hu.glosbe.com/pl/hu")
 
-driver.find_element_by_id("nemletezik")
+try:
+    element = driver.find_element_by_id("nemletezik")
 
-print("Nincs ilyen elem")
+except NoSuchElementException:
+
+    print("Field not found")
