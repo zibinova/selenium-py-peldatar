f13e43eef8a3fa2f6ba84a3e32983d91c8789892
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-06-25 12:28:45 +0200

    adding fordito.py

---
+++ python2-homework/fordito.py
@@ -0,0 +1,13 @@
+print("Kérem a pozitív számokat!, 0 vége")
+
+my_list = []
+x = int(input())
+
+while x != 0:
+    my_list.append(x)
+    x = int(input())
+
+print(my_list)
+
+print(my_list[-1:0])
+
