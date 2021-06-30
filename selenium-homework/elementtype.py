62701d01e5c6c09f5dd0328aeb7951c9233e0716
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-06-30 10:49:50 +0200

    updating elementtype.py

--- selenium-homework/elementtype.py
+++ selenium-homework/elementtype.py
@@ -4,6 +4,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://localhost:9999/trickyelements.html")
 
-driver.find_element_by_tag_name('button').click()
+driver.find_element_by_id("element1")
+driver.find_element_by_id("element2")
+driver.find_element_by_id("element3")
+driver.find_element_by_id("element4")
+driver.find_element_by_id("element5")
 
+driver.find_element_by_tag_name('button').click()
+button_label = driver.find_element_by_tag_name('button').text
+print(button_label)
+
+assert driver.find_element_by_id('result').text == (button_label + ' was clicked')
+
+driver.close()
 
