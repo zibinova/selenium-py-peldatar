67a7f4025603ca413e7a77d003e0816234e36239
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-07-04 21:32:51 +0200

    updating files with solution

--- selenium-homework/seleniumex.py
+++ selenium-homework/seleniumex.py
@@ -16,8 +16,39 @@ finally:
     pass
 driver.close()
 
+# extra próbálkozás függvénnyel
+# from selenium import webdriver
+# from selenium.common.exceptions import NoSuchElementException
+#
+# driver = webdriver.Chrome()
+# driver.get("https://hu.glosbe.com/pl/hu")
+#
+# def find(elem):
+#
+#      try:
+#      except NoSuchElementException:
+#      print("Ilyen mező nem létezik")
+#
+# finally:
+#         pass
+#
+#
+# find(nincs)
+# find("ájdi")
 
-# extra:
 
-
-#def this_sucks():
+# tanár:
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.common.exceptions import NoSuchElementException
+#
+# # In order for ChromeDriverManager to work you must pip install it in your own environment.
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+#
+# try:
+#     driver.get("https://google.com")
+#     links = driver.find_elements_by_id("no_such_element")
+# except NoSuchElementException as e:
+#     print('Element not found: ', e)
+# finally:
+#     driver.close()
--- selenium-homework/start.py
+++ selenium-homework/start.py
@@ -5,6 +5,15 @@ driver = webdriver.Chrome()
 driver.get("http://semjenhaza.hu/")
 
 
-
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# import time
+#
+# # In order for ChromeDriverManager to work you must pip install it in your own environment.
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+#
+# driver.get("https://google.com")
+# time.sleep(5)
+# driver.close()
 
 
