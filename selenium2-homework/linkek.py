1bde03e05a2b9cf9b4759c425b6d263d2b67266d
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-07-07 14:14:57 +0200

    adding linkek.py

---
+++ selenium2-homework/linkek.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+try:
+    base_url = "http://localhost:9999"
+    driver.get(base_url)
+    links = driver.find_elements_by_xpath('//a')
+    print(len(links))
+    string_links = ("".join(links))
+    print(string_links)
+
+    for i in links:
+        with open("egyfajl.txt", "w") as f:
+            f.write(i.text)
+
+except NoSuchElementException as e:
+    print('Element not found: ', e)
+finally:
+    driver.close()
+
