8715376c876494e9ced21b35e5996fd1d367b131
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-07-08 17:32:58 +0200

    update

---
+++ selenium2-homework/edittable.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import csv
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from datetime import datetime
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("http://localhost:9999/editable-table.html")
+
+add_button = driver.find_element_by_tag_name("button")
+add_button.click()
+
+added_row = driver.find_element_by_xpath("//table/tbody/tr[7]")
+
+cell_name = driver.find_element_by_xpath("//table/tbody/tr[7]/td[1]")
+
+WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//table/tbody/tr[7]/td[1]")))
+
+cell_name.send_keys("Xiaomi")
+
+
+
