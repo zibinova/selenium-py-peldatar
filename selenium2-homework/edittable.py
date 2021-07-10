3a9a3101cf85c7bc3275270df5385cad95e11e75
Novák Ibolya <ibolya-novak@hotmail.co.uk>
2021-07-10 10:40:19 +0200

    update

--- selenium2-homework/edittable.py
+++ selenium2-homework/edittable.py
@@ -14,16 +14,16 @@ driver.get("http://localhost:9999/editable-table.html")
 
 add_button = driver.find_element_by_tag_name("button")
 rows = driver.find_elements_by_xpath("//table/tbody/tr")
-new_row = driver.find_element_by_xpath("//table/tbody/tr[7]")
+# new_row = driver.find_element_by_xpath("//table/tbody/tr[7]")
 
 
 def add_and_fill_row(n, p, q, c):
     add_button.click()
 
     WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//table/tbody/tr[7]/td[1]/input")))
+                EC.element_to_be_clickable((By.XPATH, "//table/tbody/tr[-1]/td[1]/input")))
 
-    cell_name = driver.find_element_by_xpath("//table/tbody/tr[7]/td[1]/input")
+    cell_name = driver.find_element_by_xpath("//table/tbody/tr[-1]/td[1]/input")
     cell_price = driver.find_element_by_xpath("//table/tbody/tr[7]/td[2]/input")
     cell_quantity = driver.find_element_by_xpath("//table/tbody/tr[7]/td[3]/input")
     cell_category = driver.find_element_by_xpath("//table/tbody/tr[7]/td[4]/input")
